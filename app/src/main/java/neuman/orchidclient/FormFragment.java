91aed019150dac7bd04a95dc0d37225cae58c5ff
Eric Neuman <soloptimus@gmail.com>
2014-06-20 03:39:28 -0400

    Apparently I've been using android studio's built in git incorrectly.  Here's a TON of stuff that's been added or changed.  Disregard previous commit messages.

--- app/src/main/java/neuman/orchidclient/FormFragment.java
+++ app/src/main/java/neuman/orchidclient/FormFragment.java
@@ -1,18 +1,22 @@
 package neuman.orchidclient;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import org.json.JSONObject;
 
@@ -41,7 +45,7 @@ public class FormFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private List<Switch> switches = new ArrayList<Switch>();
+    private List fieldList = new ArrayList();
 
     private OnFragmentInteractionListener mListener;
 
@@ -81,30 +85,61 @@ public class FormFragment extends Fragment {
         // Inflate the layout for this fragment
         View inflatedView = inflater.inflate(R.layout.fragment_form, container, false);
 
-        //find the english button and add alistener for it
-        Button button_submit = (Button) inflatedView.findViewById(R.id.submit_button);
-        button_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                submitForm();
-            }
-        });
 
         LinearLayout layout = (LinearLayout) inflatedView.findViewById(R.id.FieldsLinearLayout);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
         String[] switch_names = {"Alpha", "Beta", "Gamma"};
         for (String name : switch_names)
         {
             Switch new_switch = new Switch(getActivity());
             new_switch.setText(name);
             new_switch.setTag(name);
-            layout.addView(new_switch,
-                    new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT));
-            switches.add(new_switch);
-        }
+            layout.addView(getLinearLayout(new_switch, ""),layoutParams);
 
+            fieldList.add(new_switch);
+        }
+        String[] text_names = {"Delta", "Epsilon", "Zeta"};
+        for (String name : text_names)
+        {
+            EditText new_edit = new EditText(getActivity());
+            new_edit.setTag(name);
+            layout.addView(getLinearLayout(new_edit, name),layoutParams);
+
+            fieldList.add(new_edit);
+        }
+        String[] multi_names = {"Eta", "Theta", "Iota"};
+        for (String name : multi_names)
+        {
+            EditText new_edit = new EditText(getActivity());
+            new_edit.setTag(name);
+            new_edit.setMaxLines(5);
+            new_edit.setLines(5);
+            new_edit.setSingleLine(false);
+            layout.addView(getLinearLayout(new_edit, name),layoutParams);
+
+            fieldList.add(new_edit);
+        }
+        Button submit_button = new Button(getActivity());
+        submit_button.setText("Submit");
+        layout.addView(submit_button, layoutParams);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
         return inflatedView;
+    }
+
+    private LinearLayout getLinearLayout(View field, String name){
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        LinearLayout outputLayout = new LinearLayout(getActivity());
+        outputLayout.setOrientation(LinearLayout.VERTICAL);
+        TextView label = new TextView(getActivity());
+        label.setText(name);
+        outputLayout.addView(label, layoutParams);
+        outputLayout.addView(field);
+        return outputLayout;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -143,12 +178,23 @@ public class FormFragment extends Fragment {
 
     private void submitForm(){
         List switch_values = new ArrayList();
-        for (Switch s : switches)
+
+        for (Object f : fieldList)
         {
-            Map valueMap = new HashMap();
-            valueMap.put("name", (String) s.getTag());
-            valueMap.put("value", s.isChecked());
-            switch_values.add(valueMap);
+            if (f instanceof Switch) {
+                Switch old_switch = (Switch) f;
+                Map valueMap = new HashMap();
+                valueMap.put("name", (String) old_switch.getTag());
+                valueMap.put("value", old_switch.isChecked());
+                switch_values.add(valueMap);
+            }
+            else if (f instanceof EditText) {
+                EditText old_edit = (EditText) f;
+                Map valueMap = new HashMap();
+                valueMap.put("name", (String) old_edit.getTag());
+                valueMap.put("value", old_edit.getText().toString());
+                switch_values.add(valueMap);
+            }
 
         }
         Map outputMap = new HashMap();
